80eff8955b60c677658f535395b6ff3e061095dc
crashbang22 <76707679+crashbang22@users.noreply.github.com>
2025-03-07 21:33:50 -0500

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -286,7 +286,7 @@ export default function Page() {
           </ScrollAnimation>
           <ScrollAnimation delay={0.4}>
             <div className="max-w-4xl mx-auto bg-black rounded-lg shadow-lg overflow-hidden border border-royal-600">
-              <CalendlyEmbed data-url="https://calendly.com/contact-insyncmarketing/30min" style="min-width:320px;height:700px;"/>
+              <CalendlyEmbed url="https://calendly.com/contact-insyncmarketing/30min"/>
             </div>
           </ScrollAnimation>
         </div>
